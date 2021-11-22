dde6249ae7ccf76e75f2614a6b2a2bf220ef2df1
MdShaurov <shaurovmd123@gmail.com>
2021-11-22 11:25:34 -0600

    Update major proposal

--- proposal.md
+++ proposal.md
@@ -1,7 +1,14 @@
 # CS30 Major Project Proposal - Md Shaurov
-
-Description:
+I am planning to make a simplified version of a game called ShellShock. The game creates a random 2D terrain map
+every time you click play. Where there are 2 tanks on either side of the 2D map. It is turn based so only one player
+aims the gun of the tank by adjusting the arc of the barrel. The player can shoot and if the other player's tank is 
+not destroyed yet, it becomes the other players turn. 
 
 Need to Have List:
+- random terrain generation
+- ability to move, aim, and shoot
+- draw 2 tanks on either sides
 
 Nice to Have List:
+- ability to shoot different types of shells
+- animated visuals
--- sketch.js
+++ sketch.js
@@ -5,27 +5,34 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let rectHeights = [];
+let numberOfRects;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  numberOfRects = width;
+  generateTerrain();
 }
 
 function draw() {
   background(220);
-  
-  rect(mouseX, mouseY, 100, 50);
+  displayTerrain();
 }
 
-class Character {
-  constructor(x, y, choice) {
-    this.x = x;
-    this.y = y;
-    this.width = 100;
-    this.height = 100;
-
-  }
-
-  display() {
-    image(characterImg, this.x, this.y);
+function displayTerrain() {
+  let theWidth = width/rectHeights.length;
+  for (let i=0; i<rectHeights.length; i++) {
+    let theHeight = rectHeights[i];
+    fill("black");
+    rect(theWidth*i, height, 10, -theHeight);
   }
 }
+function generateTerrain() {
+  let time = 0;
+  for (let i=0; i<numberOfRects; i++) {
+    let theHeight = noise(time) * height;
+    rectHeights.push(theHeight);
+    time += 0.002;
+  }
+}
+
