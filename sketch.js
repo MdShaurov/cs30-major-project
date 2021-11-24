517e7ba696e28ad99d6b92ebf41289ef35fbe64f
MdShaurov <shaurovmd123@gmail.com>
2021-11-24 12:10:08 -0600

    Start

--- sketch.js
+++ sketch.js
@@ -5,43 +5,42 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let rectHeights = [];
-let numberOfRects;
+let leftTank, rightTank;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  numberOfRects = width;
-  generateTerrain();
+
+  leftTank = new Tank(100, 700);
 }
 
 function draw() {
   background(220);
-  displayTerrain();
+
+  leftTank.update();
+  leftTank.display();
 }
 
-function displayTerrain() {
-  let theWidth = width/rectHeights.length;
-  for (let i=0; i<rectHeights.length; i++) {
-    let theHeight = rectHeights[i];
-    fill("black");
-    rect(theWidth*i, height, 10, -theHeight);
-  }
-}
 
-function generateTerrain() {
-  let time = 0;
-  for (let i=0; i<numberOfRects; i++) {
-    let theHeight = noise(time) * height;
-    rectHeights.push(theHeight);
-    time += 0.001;
-  }
-}
-
-class LeftTank {
+class Tank {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  
+  display() {
+    rect(this.x, this.y, 20, 20);
+  }
+
+  update() {
+    if (keyIsDown(68)) {
+      if (this.x >= 0) {
+        this.x++;
+      }
+    }
+    if (keyIsDown(65)) {
+      if (this.x > 0) {
+        this.x--;
+      }
+    }
+  }
 }
